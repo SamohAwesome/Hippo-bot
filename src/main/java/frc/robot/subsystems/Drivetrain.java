8e91d3699fefacb5406b35e581e221e91304a835
SamohAwesome <72154565+SamohAwesome@users.noreply.github.com>
2023-01-13 18:58:06 -0500

    Hepper

---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,31 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Drivetrain extends SubsystemBase {
+  /** Creates a new Drivetrain. */
+  private final CANSparkMax leftFrontMotor = new CANSparkMax(Constants.LEFT_WHEEL_PORT,MotorType.kBrushless);
+  private final CANSparkMax rightFrontMotor = new CANSparkMax(Constants.RIGHT_WHEEL_PORT,MotorType.kBrushless);
+  private final DifferentialDrive drivetrain;
+  public Drivetrain() {
+    drivetrain = new DifferentialDrive(leftFrontMotor,rightFrontMotor);
+    drivetrain.setMaxOutput(.25);
+  }
+//SHEEEEESH//
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+  public void drive(double leftSpeed, double rightSpeed){
+      drivetrain.tankDrive(leftSpeed, rightSpeed);
+  }
+}
